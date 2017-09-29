8ff8b33f3a98d1a1e11f2fcda6a6b6f221e848f9
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2017-09-29 11:22:31 +0300

    Create max_in_array@0.0.1.cpp

---
+++ max_in_array@0.0.1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    for( string string; getline(cin, string); ) {
+        int numbers[ 10 ];
+        istringstream stream( string );
+        bool failure = false;
+        for( int i = 0; i < 10; ++i ) {
+            if( !( stream >> numbers[ i ] ) ) {
+                failure = true;
+                break;
+                }
+            }
+        if( !failure ) {
+            for( int i = 0; i < 10; ++i ) {
+                cout << numbers[ i ] << " ";
+                }
+            }
+        else {
+            std::cout << "An error has occured while reading numbers from line" << endl;
+            }
+        int max = numbers[0];
+        for(int i = 0; i< 10; i++) {
+            if(numbers[i]>max) max=numbers[i];
+        }
+        cout<<endl<<max;
+        }
+}
